8c123620172b71691217fc05519021ca95e88972
unknown <tigulima@gmail.com>
2022-01-19 15:55:18 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <Text style={styles.text}>WELCOME TO ANJO!</Text>
+      <Image
+        source={{
+          uri: "https://anjo.mobi/wp-content/uploads/2020/06/disp_01.png",
+        }}
+        style={{
+          height: 340,
+          width: 300,
+        }}
+      />
+      <View style={styles.button}>
+        <Button
+          onPress={() => {
+            alert("oi... vc vem sempre aqui?");
+          }}
+          title="CONNECT"
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +30,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#45A6A2",
+    alignItems: "center",
+    justifyContent: "flex-end",
+  },
+  text: {
+    fontSize: 28,
+    color: "#fff",
+    marginBottom: 100,
+  },
+  button: {
+    backgroundColor: "#ECECEC",
+    paddingHorizontal: 150,
+    padding: 25,
+    borderRadius: 5,
+    marginTop: 150,
+    marginBottom: 50,
   },
 });
